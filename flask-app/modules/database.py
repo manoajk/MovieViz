e86d1b20e0224afc50d5aa659d18f0f579a69618
Sahaj Bhatt <sahajb97@gmail.com>
2017-11-19 16:22:42 -0500

    Added comments on how to use modules/database to access the db

--- flask-app/modules/database.py
+++ flask-app/modules/database.py
@@ -3,6 +3,23 @@ from flask import g
 from config import Config
 from app import app
 
+"""
+HOW TO USE:
+`from modules import database as db`
+
+`db.getCursor().execute("INSERT_SQL_COMMAND_HERE")`
+IF SELECT QUERY:
+`db.getCursor().fetchall()`
+OR IF INSERT OR UPDATE QUERY:
+`db.getDB().commit()`
+
+Example:
+If you want all movies
+`db.getCursor().execute("SELECT * FROM movie")
+movies = db.getCursor().fetchall()`
+
+"""
+
 def getDB():
     db = getattr(g, '_database', None)
     if db is None:
